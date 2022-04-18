6d72cd11324248b939e4a654abc62192113edb03
Максим Баклан <Baklan.M.ptashka@gmail.com>
2022-04-18 10:45:51 +0300

    Work on the method playGame

--- src/com/shpp/p2p/cs/mbaklan/assignment3/Assignment3Part5.java
+++ src/com/shpp/p2p/cs/mbaklan/assignment3/Assignment3Part5.java
@@ -1,4 +1,15 @@
 package com.shpp.p2p.cs.mbaklan.assignment3;
 
-public class Assignment3Part5 {
+import com.shpp.cs.a.console.TextProgram;
+
+public class Assignment3Part5 extends TextProgram {
+    public static final int SUM_FOR_FINISH = 20;
+    public static final int STARTING_SUM = 1;
+
+    public void run() {
+        playGame(STARTING_SUM, SUM_FOR_FINISH);
+    }
+
+    private void playGame(int startingSum, int sumForFinish) {
+    }
 }
